a22d83ea0e8627fade2190662a152eb35e4576fd
Marion Douet <squalaladraw@gmail.com>
2019-10-30 22:06:58 +0100

    css fonctionnalité random

--- hackalloween-app/src/components/DisplayMovies.jsx
+++ hackalloween-app/src/components/DisplayMovies.jsx
@@ -7,7 +7,7 @@ function DisplayMovies({ movies }) {
       <h1 className="movieTitle">{movies.title.replace(/_/g," ")}</h1>
       <img className="movieImg"
         src={movies.posterUrl}
-        alt={movies.title.replace(/_/g," ")}
+        alt={movies.title}
       />
       <p className="movieTxt">By {movies.director.replace(/_/g," ")}, in {movies.year}</p>
     </div>
