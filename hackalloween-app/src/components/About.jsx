e2a0c337af93905711cd8659d751206a939da1d5
Ericka Barbaux <ericka.barbaux@gmail.com>
2019-10-30 18:48:12 +0100

    Création de la page about

--- hackalloween-app/src/components/About.jsx
+++ hackalloween-app/src/components/About.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import PhotoMarion from '../images/Photomarion.jpg'
 import {Link} from 'react-router-dom'
+import "./About.css"
 
 function About () {
   return (
     
     <Link to="/about">
-    <div className="About" >
+    <div >
       <section >
-        <img src={PhotoMarion} alt="Photo développeuse"/>
+        <div className="Marion-description">
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint porro rerum, nihil aperiam asperiores ut,
+             voluptatum velit ea pariatur dolore quidem adipisci architecto nam dignissimos molestiae. Blanditiis dolorem deserunt eaque.</p>
+          <img src={PhotoMarion} alt="Photo développeuse" className="img-about" />
+        </div>
+        
       </section>      
     </div >
     </Link>
