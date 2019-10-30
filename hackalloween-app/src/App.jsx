a6931dad56059c737da3dc9d41fc1914904b4dbb
user <inesborreill@gmail.com>
2019-10-30 15:51:33 +0100

    page Home v0

---
+++ hackalloween-app/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
