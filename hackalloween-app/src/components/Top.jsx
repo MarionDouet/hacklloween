8eb0d99dec1fecfc852aed8529ccf7117e5233ba
Lucie Fontela <luciefontela@gmail.com>
2019-10-30 22:21:56 +0100

    modif top

--- hackalloween-app/src/components/Top.jsx
+++ hackalloween-app/src/components/Top.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import './Top.css';
 import {Link} from 'react-router-dom';
+import Forest from '../images/Forest - 17844.mp4';
 
 
 function Top() {
     return (
         <Link to="/top">
-            <div className="top"></div>
+            <div className="videoforest">
+                <video className='forest' autoPlay loop muted>
+                    <source src={Forest} type='video/mp4'/>
+                    <source src={Forest} type='video/ogg' />
+                </video>
+            </div>
+            <div className="top-movies" id="top-movies">
+                    <p>
+                        <a to ="/top-gore">Top gore</a>
+                    </p>
+                    <p>
+                        <a to ="/top-nanar">Top nanar</a>
+                    </p>
+                    <p>
+                        <a to ="/top-omg">Top omg</a>
+                    </p>
+
+            </div>
         </Link>
+    
+    
     );
  }
 
