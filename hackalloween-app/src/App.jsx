54c9c284642a5e54bd8268054eeb453d67b1d824
Marion Douet <squalaladraw@gmail.com>
2019-10-30 18:47:57 +0100

    ajout fonctionnalité random

---
+++ hackalloween-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RandomButton from './components/RandomButton'
+
+function App() {
+  return (
+    <div className="App">
+     <RandomButton/>
+    </div>
+  );
+}
+
+export default App;
