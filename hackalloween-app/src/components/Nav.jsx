2276f0ef281f1291559fe3d4f237c89c4be68613
Lucie Fontela <luciefontela@gmail.com>
2019-10-31 12:10:56 +0100

    page top

--- hackalloween-app/src/components/Nav.jsx
+++ hackalloween-app/src/components/Nav.jsx
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom';
 function Nav() {
   return (
     <nav className="nav">
-      <Link to="/">
+      <Link to="/" className="navlink">
         <div className="title-logo">
      <img src={Eye} alt="Website logo" className="img-nav" />
      </div>
