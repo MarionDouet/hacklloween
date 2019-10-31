f6717c121c6ea7e20673b67eedcbfebd33292f0e
Lucie Fontela <luciefontela@gmail.com>
2019-10-31 10:57:36 +0100

    modif page top

--- hackalloween-app/src/components/Top.jsx
+++ hackalloween-app/src/components/Top.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 import './Top.css';
 import {Link} from 'react-router-dom';
 import Forest from '../images/Forest - 17844.mp4';
+import ImgSaw from '../img-top/saw-3.jpg';
+import ImgCanibalHolocaust from'../img-top/Cannibal-Holocaust.jpg';
+import ImgFinalDestination from '../img-top/final-destination2.jpg';
+import ImgParanormalActivity from '../img-top/paranormal-activity.jpg';
+import ImgTheExorcist from '../img-top/The-Exorcist.jpg';
+import ImgScream from '../img-top/scream-4.jpg';
+import ImgBlairWitch from '../img-top/Blair-Witch.jpg';
+import ImgInsidious from '../img-top/insidious2.jpg';
+import ImgPsychose from '../img-top/psychose.jpg';
 
 
 function Top() {
@@ -13,18 +22,35 @@ function Top() {
                     <source src={Forest} type='video/ogg' />
                 </video>
             </div>
-            <div className="top-movies" id="top-movies">
-                    <p>
-                        <a to ="/top-gore">Top gore</a>
-                    </p>
-                    <p>
-                        <a to ="/top-nanar">Top nanar</a>
-                    </p>
-                    <p>
-                        <a to ="/top-omg">Top omg</a>
-                    </p>
+            <div >
+                <div className="top-movies">
+                    <div className="top-gore">
+                            <p>
+                                <a to ="/top-gore">Top gore</a>
+                            </p>
+                        <img src={ImgSaw} alt="images saw" className="img-top" />
+                        <img src={ImgCanibalHolocaust} alt="images cannibal holocaust" className="img-top" />
+                        <img src={ImgFinalDestination} alt="images final destination" className="img-top" />
+                    </div>   
+                    <div className="top-nanar">
 
-            </div>
+                                <p>
+                                    <a to ="/top-nanar">Top nanar</a>
+                                </p>
+                            <img src={ImgParanormalActivity} alt="images paranormal activity" className="img-top" />
+                            <img src={ImgTheExorcist} alt="images the exorcist " className="img-top" />
+                            <img src={ImgScream} alt="images scream " className="img-top" />
+                    </div>
+                    <div className="top-omg">
+                                <p>
+                                    <a to ="/top-omg">Top omg</a>
+                                </p>
+                            <img src={ImgBlairWitch} alt="images blair witch" className="img-top" />
+                            <img src={ImgInsidious} alt="images insidious " className="img-top" />
+                            <img src={ImgPsychose} alt="images psychose " className="img-top" />
+                    </div>
+                </div>
+             </div>
         </Link>
     
     
