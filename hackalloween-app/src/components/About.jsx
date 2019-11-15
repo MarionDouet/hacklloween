438f8b93d9c965f30ff706cedaa9eef61bb34b35
Ericka Barbaux <ericka.barbaux@gmail.com>
2019-11-15 14:01:55 +0100

    Ok

--- hackalloween-app/src/components/About.jsx
+++ hackalloween-app/src/components/About.jsx
@@ -19,21 +19,21 @@ function About () {
      
       <div className="description">
            <p> Hello, I'm Inès. I love watching horror movies. But I don’t like watching them alone. My favorite horror movies are The circle, Shinning and SAW.</p>
-          <img src={PhotoInes} alt="Photo développeuse" className="img-about" />
+          <img src={PhotoInes} alt="Photo_développeuse" className="img-about" />
+        </div>
+        
+        <div className="description">
+           <p> Hello, i'm ericka I'm web developer in Js, happy halloween !!! The first horror film I saw was 'the circle'. I couldn't sleep for the next three days. ^^</p>
+          <img src={PhotoEricka} alt="Photo_éveloppeuse" className="img-about" />
+        </div>
+        <div className="description">
+           <p>Hello, je m'apelle luciee je suis dev web depuis peu, j'ai peur de tout , je suis une vrai flipette  </p>
+          <img src={PhotoLucie} alt="Photo_développeuse" className="img-about" />
         </div>
         <div className="description">
            <p> Redrum rEdrum reDrum redRum redrUm redruM Redrum rEdrum reDrum redRum redrUm redruM Redrum rEdrum reDrum redRum redrUm redruM Redrum rEdrum reDrum redRum redrUm redruM</p>
-          <img src={PhotoMarion} alt="Photo développeuse" className="img-about" />
+          <img src={PhotoMarion} alt="Photo_développeuse" className="img-about" />
         </div>
-        <div className="description">
-           <p> Hello, i'm ericka I'm web developer in Js, happy halloween !!! The first horror film I saw was 'the circle'. I couldn't sleep for the next three days. ^^</p>
-          <img src={PhotoEricka} alt="Photo développeuse" className="img-about" />
-        </div>
-        <div className="description">
-           <p>Hello, je m'apelle luciee je suis </p>
-          <img src={PhotoLucie} alt="Photo développeuse" className="img-about" />
-        </div>
-        
         
       </section>      
     </div >
--- hackalloween-app/src/components/Nav.jsx
+++ hackalloween-app/src/components/Nav.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import './Nav.css';
 import Eye from '../images/Eye.png';
-import {Link} from 'react-router-dom';
-import sound from '../images/bruit.mp3'
+import { NavLink } from 'react-router-dom';
+import sound from '../images/bruit.mp3';
+
 
 
 function Nav() {
@@ -11,19 +12,19 @@ function Nav() {
 
   return (
     <nav className="nav">
-      <Link to="/">
+      <NavLink to="/" className="alink" >
         <div className="title-logo">
-     <img src={Eye} alt="Website logo" className="img-nav" />
-     </div>
-      
-      <h1 className="title-site">Fear focus</h1>
-      </Link>
+          <img src={Eye} alt="Website logo" className="img-nav" />
+        </div>
+
+        <h1 className="title-site">Fear focus</h1>
+      </NavLink>
       <div className="about-contact">
         <p>
-          <Link to ="/about">About</Link>
+          <NavLink to="/about" >About</NavLink>
         </p>
         <p>
-          <Link to ="/top">Top</Link>
+          <NavLink to="/top" >Top</NavLink>
         </p>
       </div>
     </nav>
